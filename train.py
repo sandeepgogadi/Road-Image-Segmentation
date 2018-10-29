52f00464e183278103538d4b8655cc2f1fbfb0e8
sandeepgogadi <sandeepgogadi@yahoo.com>
2018-10-29 09:13:00 -0700

    icnet

--- train.py
+++ train.py
@@ -91,8 +91,8 @@ else:
 
 # Optimizer
 # optimizer = tf.train.AdamOptimizer(args.lr)
-# optimizer = tf.keras.optimizers.Adam(args.lr)
-optimizer = tf.keras.optimizers.SGD(lr=args.lr, momentum=0.9)
+optimizer = tf.keras.optimizers.Adam(args.lr)
+# optimizer = tf.keras.optimizers.SGD(lr=args.lr, momentum=0.9)
 print('Optimizer selected')
 # Use tf.keras.optimizers.Adam when keras for tpu comes out of
 # experimental.
