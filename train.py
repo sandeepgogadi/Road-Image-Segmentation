91008dc0b56860dbeeca1a405c3b8ecb54fa86a8
sandeepgogadi <sandeepgogadi@yahoo.com>
2018-10-28 23:06:50 -0700

    icnet

--- train.py
+++ train.py
@@ -75,7 +75,8 @@ print('loaded loss weights')
 
 # Optimizer
 # optimizer = tf.train.AdamOptimizer(args.lr)
-optimizer = tf.keras.optimizers.Adam(args.lr)
+# optimizer = tf.keras.optimizers.Adam(args.lr)
+optimizer = tf.keras.optimizers.SGD(lr=args.lr, momentum=0.9)
 print('Optimizer selected')
 # Use tf.keras.optimizers.Adam when keras for tpu comes out of
 # experimental.
