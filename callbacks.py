d0bfc5453f2c421370b03213fff0782df21e9674
sandeepgogadi <sandeepgogadi@yahoo.com>
2018-10-29 12:41:14 -0700

    icnet

--- callbacks.py
+++ callbacks.py
@@ -17,13 +17,15 @@ class PolyDecay:
 def get_callbacks(args):
 
     checkpoint = tf.keras.callbacks.ModelCheckpoint('weights/weights_{}.h5'.format(args.net),
-                                                    monitor='val_conv6_cls_categorical_accuracy', mode='max', save_weights_only=True,
+                                                    monitor='val_conv6_cls_categorical_accuracy',
+                                                    mode='max', save_weights_only=True,
                                                     save_best_only=True, verbose=1)
 
     stopping = tf.keras.callbacks.EarlyStopping(
         monitor='val_conv6_cls_categorical_accuracy', patience=10)
 
-    lr_decay = tf.keras.callbacks.LearningRateScheduler(PolyDecay(args.lr, 0.9, 10).scheduler)
+    lr_decay = tf.keras.callbacks.LearningRateScheduler(
+        PolyDecay(args.lr, 0.9, args.epochs).scheduler)
 
     #reduce_lr = tf.keras.callbacks.ReduceLROnPlateau()
 
