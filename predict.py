0a1eb5ee3c788e6dde04d9cfe8b05a88b5a0af0e
sandeepgogadi <sandeepgogadi@yahoo.com>
2018-11-02 18:07:29 -0700

    icnet

--- predict.py
+++ predict.py
@@ -11,10 +11,12 @@ from utils import prepare_repo
 from label import labels
 from matplotlib import pyplot as plt
 import numpy as np
+import glob
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--net', type=str, help='From a selection of ICNET.')
+parser.add_argument('--net', type=str, default='ICNET', help='From a selection of ICNET.')
 parser.add_argument('--file', type=str, help='File path for the image.')
+parser.add_argument('--folder', type=str, default='Images/', help='Folder path for the image.')
 
 # parser.add_argument('', type=int, default=, help='')
 
@@ -44,53 +46,56 @@ if args.net == 'ICNET':
 model = models.get_model(args.net, width, height, num_classes, weights_path)
 print('loaded model')
 
-# Load and resize image
-img = cv2.imread(args.file, 1)
-img = cv2.resize(img, (width, height))
-img_reshape = img.reshape(-1, *img.shape)
+# Images list
+if args.file:
+    images_list = [args.file]
+else:
+    images_list = glob.glob(args.folder + '*')
+
+num_images = len(images_list)
+print('{} images found.'.format(num_images))
 
 # Predict
 start_time = time.time()
-pred = model.predict(img_reshape, batch_size=1)
-duration = time.time() - start_time
-print('Generated segmentations in %s seconds -- %s FPS' % (duration, 1.0/duration))
-
-# Classes in image
-pred_img = np.argmax(pred[0], axis=-1)
-print(pred_img.shape)
-unique = np.unique(pred_img)
-print(unique)
-
-# Prepare color image
-col_img = np.zeros((*pred_img.shape, 3), dtype=np.uint8)
-
-for label in labels:
-    color = label.color
-    trainId = label.trainId
-    col_img[pred_img == trainId] = color
-
-# plt.imshow(col_img)
-# plt.axis('off')
-# plt.show()
-
-# Show images
-rgb = img[..., ::-1]
-rgb = cv2.resize(rgb, (col_img.shape[1], col_img.shape[0]))
 
 # plt.figure(figsize=(9, 14))
 plt.figure()
 
-plt.subplot(1, 2, 1)
-plt.imshow(rgb)
-plt.axis('off')
+for count, image_file in enumerate(images_list):
 
-plt.subplot(1, 2, 2)
-plt.imshow(col_img)
-plt.axis('off')
+    # Load and resize image
+    img = cv2.imread(image_file, 1)
+    img = cv2.resize(img, (width, height))
+    img_reshape = img.reshape(-1, *img.shape)
+
+    pred = model.predict(img_reshape, batch_size=1)
+
+    # Classes in image
+    pred_img = np.argmax(pred[0], axis=-1)
+    unique = np.unique(pred_img)
+
+    # Prepare color image
+    col_img = np.zeros((*pred_img.shape, 3), dtype=np.uint8)
+
+    for label in labels:
+        color = label.color
+        trainId = label.trainId
+        col_img[pred_img == trainId] = color
+
+    # Show images
+    rgb = img[..., ::-1]
+    rgb = cv2.resize(rgb, (col_img.shape[1], col_img.shape[0]))
+
+    plt.subplot(num_images, 2, 2*count+1)
+    plt.imshow(rgb)
+    plt.axis('off')
+
+    plt.subplot(num_images, 2, 2*count+2)
+    plt.imshow(col_img)
+    plt.axis('off')
+
+duration = time.time() - start_time
+print('Generated segmentations for %s images in %s seconds -- %s FPS' %
+      (num_images, duration, num_images/duration))
 
 plt.show()
-
-# Save images
-cv2.imwrite('outputs/test.png', img)
-plt.imsave('outputs/rgb.png', rgb)
-plt.imsave('outputs/color.png', col_img)
